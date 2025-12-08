70b1410312101aaaa0ba519575ad3ad6a6be1bb7
John Doe <tauhaitac3k@gmail.com>
2025-12-08 08:18:48 +0700

    Add 455 Assign Cookies in leetcode

---
+++ 455. Assign Cookies/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        while(i < g.length && j < s.length){
+            if(s[j] >= g[i]){
+                i++;
+            }j ++;
+        }
+        return i;
+    }
+}
