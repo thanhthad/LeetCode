b0e45acf35cb7c9a338bb8f6a987cbd0d8e05e6f
John Doe <tauhaitac3k@gmail.com>
2025-12-29 22:48:22 +0700

    Add 100 Same Tree in leetcode

---
+++ 100. Same Tree/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+     public static void main(String[] args) {
+
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
