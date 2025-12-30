faa3cd6c72d581c8f3ed2cbbf6bc315fd2b689fe
John Doe <tauhaitac3k@gmail.com>
2025-12-30 10:06:34 +0700

    Add 111. Minimum Depth of Binary Tree in leetcode

---
+++ 111. Minimum Depth of Binary Tree/src/Main.java
@@ -0,0 +1,42 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if(left == 0 ){
+            return 1 + right;
+        }
+        else if(right == 0 ){
+            return 1 + left;
+        }
+        else{
+            return 1 + Math.min(left,right);
+        }
+    }
+    public static void main(String[] args) {
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
